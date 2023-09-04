cfcdc3cd8ac2da67eb6f8740f232ac61bb846c34
ahmedhossam1212 <pmedo1689@gmail.com>
2023-09-04 19:26:09 +0300

    final session yossef & adam

--- lib/model.dart
+++ lib/model.dart
@@ -1,19 +1,41 @@
-class MoviesData {
+class MovieData {
+  MovieData({
+    required this.page,
+    required this.results,
+    required this.totalPages,
+    required this.totalResults,
+  });
   late final int page;
-  late final List<Movie> list;
+  late final List<Results> results;
   late final int totalPages;
   late final int totalResults;
 
-  MoviesData.fromJson(Map<String, dynamic> json) {
-    page = json['page'] ?? 0;
-    list =
-        List.from(json['results'] ?? []).map((e) => Movie.fromJson(e)).toList();
-    totalPages = json['total_pages'] ?? 0;
-    totalResults = json['total_results'] ?? 0;
+  MovieData.fromJson(Map<String, dynamic> json) {
+    page = json['page'];
+    results =
+        List.from(json['results']).map((e) => Results.fromJson(e)).toList();
+    totalPages = json['total_pages'];
+    totalResults = json['total_results'];
   }
 }
 
-class Movie {
+class Results {
+  Results({
+    required this.adult,
+    required this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
   late final bool adult;
   late final String backdropPath;
   late final List<int> genreIds;
@@ -21,36 +43,31 @@ class Movie {
   late final String originalLanguage;
   late final String originalTitle;
   late final String overview;
-  late final num popularity;
+  late final double popularity;
   late final String posterPath;
   late final String releaseDate;
   late final String title;
   late final bool video;
-  late final num voteAverage;
+  late final double? voteAverage;
   late final int voteCount;
 
-  Movie.fromJson(Map<String, dynamic> json) {
-    adult = json['adult'] ?? false;
-    if (json['backdrop_path'] == null) {
-      backdropPath =
-          "https://filestore.community.support.microsoft.com/api/images/ext?url=https%3A%2F%2Fanswerscdn.microsoft.com%2Fstatic%2Fimages%2Fimage-not-found.jpg";
-    } else {
-      backdropPath =
-          "https://image.tmdb.org/t/p/original${json['backdrop_path']}";
-    }
+  Results.fromJson(Map<String, dynamic> json) {
+    adult = json['adult'];
 
-    genreIds = List.castFrom<dynamic, int>(json['genre_ids'] ?? []);
-    id = json['id'] ?? 0;
-    originalLanguage = json['original_language'] ?? "";
-    originalTitle = json['original_title'] ?? "";
-    overview = json['overview'] ?? "";
-    popularity = json['popularity'] ?? 0;
-    posterPath = json['poster_path'] ??
-        "https://filestore.community.support.microsoft.com/api/images/ext?url=https%3A%2F%2Fanswerscdn.microsoft.com%2Fstatic%2Fimages%2Fimage-not-found.jpg";
-    releaseDate = json['release_date'] ?? "";
-    title = json['title'] ?? "";
-    video = json['video'] ?? false;
-    voteAverage = json['vote_average'] ?? 0;
-    voteCount = json['vote_count'] ?? 0;
+    backdropPath =
+        "https://image.tmdb.org/t/p/original${json['backdrop_path']}";
+
+    genreIds = List.castFrom<dynamic, int>(json['genre_ids']);
+    id = json['id'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    overview = json['overview'];
+    popularity = json['popularity'];
+    posterPath = json['poster_path'];
+    releaseDate = json['release_date'];
+    title = json['title'];
+    video = json['video'];
+    voteAverage = json['vote_average'];
+    voteCount = json['vote_count'];
   }
 }
